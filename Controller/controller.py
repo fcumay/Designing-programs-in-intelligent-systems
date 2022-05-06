b361606da7c1a0e8fa18f35aea57ed99a815351b
fcumay <voyshnism@mail.ru>
2022-05-06 11:55:39 +0300

    fix upload file()

--- Controller/controller.py
+++ Controller/controller.py
@@ -5,13 +5,15 @@ from Model.model import Model
 
 class Controller:
 
-    def upload_file(self,file_name):
-        return(Model.upload_file(Model,file_name))
-    def save_file(self,file_name="new.xml"):
-        Model.save_file(Model,file_name)
-    def update(self,navigation):
-        information=Model.update(Model,navigation)
+    def upload_file(self, file_name):
+        if Model.file_name_validation(Model,file_name):
+            Model.upload_file(Model, file_name)
+        return Model.file_name_validation(Model,file_name)
+
+    def save_file(self, file_name="new.xml"):
+        Model.save_file(Model, file_name)
+
+    def update(self, navigation):
+        information = Model.update(Model, navigation)
         self.save_file(self)
         return information
-
-
--- Model/model.py
+++ Model/model.py
@@ -7,20 +7,21 @@ import math
 class Model:
     curr_page=0
     information = {'full_name': [], 'group': [], 'valid_reason': [], 'invalid_reason': [], 'totall': []}
-    def upload_file(self,file_name):
-        if os.path.exists(file_name):
-            handler = read.Reader()
-            handler.parser.setContentHandler(handler)
-            handler.parser.parse(file_name)
 
-            self.information['full_name'] = handler.student_full_name
-            self.information['group'] = handler.student_group
-            self.information['valid_reason'] = [int(x) for x in handler.student_valid_reason]
-            self.information['invalid_reason'] = [int(x) for x in handler.student_invalid_reason]
-            self.information['totall'] = list(map(sum, zip(Model.information['valid_reason'], Model.information['invalid_reason'])))
-            return True
-        else:
-            return False
+    def file_name_validation(self,file_name):
+        return os.path.exists(file_name)
+
+    def upload_file(self,file_name):
+        handler = read.Reader()
+        handler.parser.setContentHandler(handler)
+        handler.parser.parse(file_name)
+
+        self.information['full_name'] = handler.student_full_name
+        self.information['group'] = handler.student_group
+        self.information['valid_reason'] = [int(x) for x in handler.student_valid_reason]
+        self.information['invalid_reason'] = [int(x) for x in handler.student_invalid_reason]
+        self.information['totall'] = list(map(sum, zip(Model.information['valid_reason'], Model.information['invalid_reason'])))
+
     def save_file(self,file_name):
         write.make_xml(self.information).writexml(open(file_name, 'w', encoding='utf-8'),
                                                    indent="   ",
