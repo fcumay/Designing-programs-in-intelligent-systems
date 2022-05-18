34764d85f5c8d0e6cc1e2a5e6540c5462b36f8f2
fcumay <voyshnism@mail.ru>
2022-05-18 19:54:22 +0300

    added inos made the move

--- lab3/controls.py
+++ lab3/controls.py
@@ -1,39 +1,66 @@
 import pygame
 import sys
 from bullet import Bullet
+from ino import Ino
 
-def events(screen,gun,bullets):
+
+def events(screen, gun, bullets):
     '''Computing events'''
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            #right
+            # right
             if event.key == pygame.K_d:
                 gun.mright = True
             elif event.key == pygame.K_a:
                 gun.mleft = True
             elif event.key == pygame.K_SPACE:
-                new_bullet=Bullet(screen,gun)
+                new_bullet = Bullet(screen, gun)
                 bullets.add(new_bullet)
         elif event.type == pygame.KEYUP:
-            #right
+            # right
             if event.key == pygame.K_d:
-                gun.mright=False
+                gun.mright = False
             elif event.key == pygame.K_a:
-                gun.mleft=False
+                gun.mleft = False
 
-def update(bg_color,screen,gun,bullets):
+
+def update(bg_color, screen, gun, inos, bullets):
     '''update screen'''
     screen.fill(bg_color)
     for bullet in bullets.sprites():
         bullet.draw_bullet()
     gun.output()
+    inos.draw(screen)
     pygame.display.flip()
+
 
 def update_bullets(bullets):
     '''update bullets position'''
     bullets.update()
     for bullet in bullets.copy():
-        if bullet.rect.bottom <=0:
+        if bullet.rect.bottom <= 0:
             bullets.remove(bullet)
+
+def update_inos(inos):
+    '''update inos position'''
+    inos.update()
+
+
+def create_army(screen, inos):
+    '''create army of inos'''
+    ino = Ino(screen)
+    ino_width = ino.rect.width
+    number_ino_x = int((700 - 2 * ino_width) / ino_width)
+    ino_height = ino.rect.height
+    number_ino_y = int((750 - 100 - 2 * ino_height) / ino_height)
+
+    for row_number in range(number_ino_y-1):
+        for ino_number in range(number_ino_x):
+            ino = Ino(screen)
+            ino.x = ino_width + (ino_width * ino_number)
+            ino.y = ino_height + (ino_height * row_number)
+            ino.rect.x = ino.x
+            ino.rect.y = ino.rect.height + ino.rect.height * row_number
+            inos.add(ino)
---
+++ lab3/ino.py
@@ -0,0 +1,25 @@
+import pygame
+
+
+class Ino(pygame.sprite.Sprite):
+    '''One ino'''
+
+    def __init__(self, screen):
+        '''init and start position'''
+        super(Ino, self).__init__()
+        self.screen = screen
+        self.image = pygame.image.load('images/ino.png')
+        self.rect = self.image.get_rect()
+        self.rect.x = self.rect.width
+        self.rect.y = self.rect.height
+        self.x = float(self.rect.x)
+        self.y = float(self.rect.y)
+
+    def draw(self):
+        '''Output ino on the screen'''
+        self.screen.blit(self.image,self.rect)
+
+    def update(self):
+        '''move inos'''
+        self.y+=0.1
+        self.rect.y=self.y
