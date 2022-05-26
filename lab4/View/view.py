0f377ee5a3a895982f85ad6a4e4bae70b79ee86a
fcumay <voyshnism@mail.ru>
2022-05-26 09:54:43 +0300

    fix EmptyString bug load unload

--- lab4/View/view.py
+++ lab4/View/view.py
@@ -3,6 +3,7 @@ import os.path
 from Controller.controller import Controller
 
 from kivy.config import Config
+
 Config.set("graphics", "resizable", 0)
 Config.set("graphics", "width", 800)
 Config.set("graphics", "height", 600)
@@ -13,64 +14,66 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.core.window import Window
 from View.ui import KV
 
-
-
 Builder.load_string(KV)
 
+
 class FirstPage(Screen):
-    def __init__(self,controller):
+    def __init__(self, controller):
         super(FirstPage, self).__init__()
-        self.controller=controller
+        self.controller = controller
+
     def new_game(self):
         self.controller.new_game()
+
     def continue_game(self):
         self.controller.continue_game()
 
 
 class MainPage(Screen):
-    def __init__(self,controller):
+    def __init__(self, controller):
         super(MainPage, self).__init__(name='main')
-        self.controller=controller
+        self.controller = controller
+
     def curr_station(self):
         pass
+
     def update(self):
-            self.ids['train_goods'].text = f'={self.controller.update()[-1]}= Train: :  {self.controller.update()[-2]}'
-            self.ids['st_A_goods'].text = f'Station A:  {self.controller.update()[0]}'
-            self.ids['st_B_goods'].text = f'Station B:  {self.controller.update()[1]}'
-            self.ids['st_C_goods'].text = f'Station C:  {self.controller.update()[2]}'
-            self.ids['st_D_goods'].text = f'Station D:  {self.controller.update()[3]}'
-            self.ids['st_E_goods'].text = f'Station E:  {self.controller.update()[4]}'
-            self.ids['st_F_goods'].text = f'Station F:  {self.controller.update()[5]}'
-    def next_station(self,station):
-        if station!='':
+        self.ids['train_goods'].text = f'={self.controller.update()[-1]}= Train: :  {self.controller.update()[-2]}'
+        self.ids['st_A_goods'].text = f'Station A:  {self.controller.update()[0]}'
+        self.ids['st_B_goods'].text = f'Station B:  {self.controller.update()[1]}'
+        self.ids['st_C_goods'].text = f'Station C:  {self.controller.update()[2]}'
+        self.ids['st_D_goods'].text = f'Station D:  {self.controller.update()[3]}'
+        self.ids['st_E_goods'].text = f'Station E:  {self.controller.update()[4]}'
+        self.ids['st_F_goods'].text = f'Station F:  {self.controller.update()[5]}'
+
+    def next_station(self, station):
+        if station != '':
             self.controller.next_station(int(station))
         self.update()
-    def load(self,num):
-        self.controller.load(int(num))
+
+    def load(self, num):
+        if num != '':
+            self.controller.load(int(num))
         self.update()
-    def unload(self,num):
-        self.controller.unload(int(num))
+
+    def unload(self, num):
+        if num != '':
+            self.controller.unload(int(num))
         self.update()
-
-
-
-
-        
-
 
 
 class TestApp(App):
-    def __init__(self,controller):
+    def __init__(self, controller):
         super(TestApp, self).__init__()
-        self.controller=controller
+        self.controller = controller
 
     Window.clearcolor = (.45, .76, .76, 1)
+
     def build(self):
         self.title = "Thomas"
         sm = ScreenManager()
 
         sm.add_widget(FirstPage(self.controller))
         sm.add_widget(MainPage(self.controller))
-
 
         return sm
